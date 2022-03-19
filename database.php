57f6bea4ee90ec6bc6a0a9eb6d61e7ba5f8784f0
isaacorsaah <d00234552@student.dkit.ie>
2022-03-19 08:20:31 +0000

    update

--- database.php
+++ database.php
@@ -1,10 +1,10 @@
 <?php
-    // $dsn = 'mysql:host=localhost;dbname=ca2';
-    // $username = 'root';
-    // $password = '';
-   $dsn = 'mysql:host=localhost;dbname=D00234552';
-    $username = 'D00234552';
-    $password = '0be7QRcN';
+    $dsn = 'mysql:host=localhost;dbname=ca2';
+    $username = 'root';
+    $password = '';
+//    $dsn = 'mysql:host=localhost;dbname=D00234552';
+//     $username = 'D00234552';
+//     $password = '0be7QRcN';
 
     try {
         $db = new PDO($dsn, $username, $password);
